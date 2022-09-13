d12212628c6817ef84ce2487cd7200734d9a09d2
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-13 17:15:57 +0530

    UC1 completed...

---
+++ SnakeandLadder.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class SnakeandLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake and Ladder Game");
+        int player_position = 0;
+        System.out.println("Starting Position of Player = " + player_position);
+    }
+}
