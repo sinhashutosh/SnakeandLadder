a70e2dc1cdf5637fb6d79f673686eed1e8cb8fc9
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-13 17:33:24 +0530

    UC2 Completed

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.util.Random;
+
 public class SnakeandLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game");
         int player_position = 0;
         System.out.println("Starting Position of Player = " + player_position);
+        System.out.println("=========================================================\n");
+        int dice = dice_roll();
+        System.out.println(dice+" comes");
+    }
+
+    public static int dice_roll() {
+        int dice;
+        System.out.println("Please Roll the Dice");
+        Random rd = new Random();
+        dice = 1+rd.nextInt(6);
+        return dice;
     }
 }
